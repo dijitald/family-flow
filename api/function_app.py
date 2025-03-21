ec08c0378e7c64dd537efa0fc7ff0bdf4d04eb2f
dijitald <drew.dials@outlook.com>
2025-03-21 10:05:53 -0400

    step 0.2

--- api/function_app.py
+++ api/function_app.py
@@ -1,15 +1,15 @@
 import azure.functions as func
 import logging, os
-from sqlalchemy.orm import sessionmaker, scoped_session
-from service_models import engine
+# from sqlalchemy.orm import sessionmaker, scoped_session
+# from service_models import engine
 from function_app_context import context
-from service_households import bpHouseholds
-from service_users import bpUsers
-from service_tasks import bpTasks
-from service_activities import bpActivities
-from service_memberships import bpMembers
+# from service_households import bpHouseholds
+# from service_users import bpUsers
+# from service_tasks import bpTasks
+# from service_activities import bpActivities
+# from service_memberships import bpMembers
 from azure.monitor.opentelemetry import configure_azure_monitor
-from opentelemetry.instrumentation.sqlalchemy import SQLAlchemyInstrumentor
+# from opentelemetry.instrumentation.sqlalchemy import SQLAlchemyInstrumentor
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
