fa03ca0c209837be5a407eaecc03ff046e6c1c78
dijitald <drew.dials@outlook.com>
2025-03-21 11:30:23 -0400

    step 1

--- api/function_app.py
+++ api/function_app.py
@@ -1,14 +1,14 @@
 import azure.functions as func
 import logging, os
-# from sqlalchemy.orm import sessionmaker, scoped_session
-# from service_models import engine
 from function_app_context import context
-# from service_households import bpHouseholds
-# from service_users import bpUsers
-# from service_tasks import bpTasks
-# from service_activities import bpActivities
-# from service_memberships import bpMembers
+from service_households import bpHouseholds
+from service_users import bpUsers
+from service_tasks import bpTasks
+from service_activities import bpActivities
+from service_memberships import bpMembers
 from azure.monitor.opentelemetry import configure_azure_monitor
+# from service_models import engine
+# from sqlalchemy.orm import sessionmaker, scoped_session
 # from opentelemetry.instrumentation.sqlalchemy import SQLAlchemyInstrumentor
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
@@ -16,15 +16,15 @@ app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 configure_azure_monitor(logger_name="familyflow")
 context.logging = logging.getLogger("familyflow")  
 
-# try:
-#     app.register_blueprint(bpHouseholds)
-#     app.register_blueprint(bpUsers)
-#     app.register_blueprint(bpTasks)
-#     app.register_blueprint(bpActivities)
-#     app.register_blueprint(bpMembers)
-# except Exception as e:
-#     context.logging.critical(f"Error initializing application: {e}")
-#     raise e
+try:
+    app.register_blueprint(bpHouseholds)
+    app.register_blueprint(bpUsers)
+    app.register_blueprint(bpTasks)
+    app.register_blueprint(bpActivities)
+    app.register_blueprint(bpMembers)
+except Exception as e:
+    context.logging.critical(f"Error initializing application: {e}")
+    raise e
 
 # try:
 #     SQLAlchemyInstrumentor().instrument(engine=engine, service="familyflow")
