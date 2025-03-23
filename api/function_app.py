7e3aad57d6fad2c0c159fc305740ce5d7ff57203
dijitald <drew.dials@outlook.com>
2025-03-23 16:37:48 -0400

    nope

--- api/function_app.py
+++ api/function_app.py
@@ -1,42 +1,18 @@
 import azure.functions as func
-import logging, os
-from function_app_context import context
-from service_users import bpUsers
-from service_households import bpHouseholds
-from service_tasks import bpTasks
-from service_activities import bpActivities
-from service_memberships import bpMembers
-# from service_models import engine
+import logging
 from azure.monitor.opentelemetry import configure_azure_monitor
-# from sqlalchemy.orm import sessionmaker, scoped_session
 # from opentelemetry.instrumentation.sqlalchemy import SQLAlchemyInstrumentor
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
-configure_azure_monitor(logger_name="familyflow")
-context.logging = logging.getLogger("familyflow")  
-
-try:
-    # SQLAlchemyInstrumentor().instrument(engine=engine, service="familyflow")
-    # factory = sessionmaker(bind=engine) 
-    # session = scoped_session(factory)
-    # context.session = session
-    context.KEY = os.getenv("DEBUGKEY")
-except Exception as e:
-    context.logging.critical(f"Error initializing database: {e}")
-    raise e
-
-try:
-    # app.register_blueprint(bpHouseholds)
-    app.register_blueprint(bpUsers)
-    # app.register_blueprint(bpTasks)
-    # app.register_blueprint(bpActivities)
-    # app.register_blueprint(bpMembers)
-except Exception as e:
-    context.logging.critical(f"Error initializing application: {e}")
-    raise e
+configure_azure_monitor(
+    logger_name="familyflow",  # Set the namespace for the logger in which you would like to collect telemetry for if you are collecting logging telemetry. This is imperative so you do not collect logging telemetry from the SDK itself.
+)
+logger = logging.getLogger("familyflow")  # Logging telemetry will be collected from logging calls made with this logger and all of it's children loggers.
 
 @app.route(route="ping", methods=["GET"])
 def ping(req: func.HttpRequest) -> func.HttpResponse:
-    context.logging.info('Python HTTP trigger function processed a request.')
+    logging.info('1 Python HTTP trigger function processed a request.')
+    logger.info('2 Python HTTP trigger function processed a request.')
     return func.HttpResponse("pong", status_code=200)
+
---
+++ api/function_app_full.py
@@ -0,0 +1,42 @@
+import azure.functions as func
+import logging, os
+from function_app_context import context
+from service_users import bpUsers
+from service_households import bpHouseholds
+from service_tasks import bpTasks
+from service_activities import bpActivities
+from service_memberships import bpMembers
+# from service_models import engine
+from azure.monitor.opentelemetry import configure_azure_monitor
+# from sqlalchemy.orm import sessionmaker, scoped_session
+# from opentelemetry.instrumentation.sqlalchemy import SQLAlchemyInstrumentor
+
+app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
+
+configure_azure_monitor(logger_name="familyflow")
+context.logging = logging.getLogger("familyflow")  
+
+try:
+    # SQLAlchemyInstrumentor().instrument(engine=engine, service="familyflow")
+    # factory = sessionmaker(bind=engine) 
+    # session = scoped_session(factory)
+    # context.session = session
+    context.KEY = os.getenv("DEBUGKEY")
+except Exception as e:
+    context.logging.critical(f"Error initializing database: {e}")
+    raise e
+
+try:
+    # app.register_blueprint(bpHouseholds)
+    app.register_blueprint(bpUsers)
+    # app.register_blueprint(bpTasks)
+    # app.register_blueprint(bpActivities)
+    # app.register_blueprint(bpMembers)
+except Exception as e:
+    context.logging.critical(f"Error initializing application: {e}")
+    raise e
+
+@app.route(route="ping", methods=["GET"])
+def ping(req: func.HttpRequest) -> func.HttpResponse:
+    context.logging.info('Python HTTP trigger function processed a request.')
+    return func.HttpResponse("pong", status_code=200)
