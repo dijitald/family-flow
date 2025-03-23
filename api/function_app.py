67af69b8d41b5e37c6095d18b533ef87a36e6fd4
dijitald <drew.dials@outlook.com>
2025-03-23 15:36:35 -0400

    missed one

--- api/function_app.py
+++ api/function_app.py
@@ -7,7 +7,7 @@ from service_tasks import bpTasks
 from service_activities import bpActivities
 from service_memberships import bpMembers
 # from service_models import engine
-# from azure.monitor.opentelemetry import configure_azure_monitor
+from azure.monitor.opentelemetry import configure_azure_monitor
 # from sqlalchemy.orm import sessionmaker, scoped_session
 # from opentelemetry.instrumentation.sqlalchemy import SQLAlchemyInstrumentor
 
